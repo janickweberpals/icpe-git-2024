0979a04690ec5fa2d212e1d9a40a79b48770ef64
Janick Weberpals <janick.weberpals@me.com>
2024-06-09 14:29:02 -0400

    get pandoc location

--- update_README.R
+++ update_README.R
@@ -2,4 +2,7 @@
 library(pandoc)
 library(rmarkdown)
 
+# get pandoc location
+print(Sys.getenv("RSTUDIO_PANDOC"))
+# Sys.setenv(RSTUDIO_PANDOC="--- insert directory here ---")
 rmarkdown::render(input = "index.qmd", output_file = here::here('README.md'), output_format = 'md_document')
