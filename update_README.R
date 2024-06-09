6c7e866e58e0e33a848e7c8c7144325d1b8193b0
Janick Weberpals <janick.weberpals@me.com>
2024-06-09 14:36:15 -0400

    try different way

--- update_README.R
+++
@@ -1,8 +0,0 @@
-# update README from index.qmd
-library(pandoc)
-library(rmarkdown)
-
-# get pandoc location
-Sys.setenv(RSTUDIO_PANDOC='/usr/lib/rstudio/bin/pandoc')
-
-rmarkdown::render(input = "index.qmd", output_file = here::here('README.md'), output_format = 'md_document')
